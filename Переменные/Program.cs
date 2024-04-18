aa97d1f5a61c994fa20559316f01e62d3d76686d
KirKiller <KirKiller@DESKTOP-1JIA97R>
2024-04-19 00:11:50 +0500

    Добавьте файлы проекта.

---
+++ Переменные/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Переменные
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold, platinum, silver, titanium, zinc, lithium;
+            float silicon;
+            double manganese;
+            string name;
+            bool alloy;
+            gold = 20;
+            platinum = 34;
+            silver = 21;
+            titanium = 10;
+            zinc = 6;
+            lithium = 5;
+            silicon=2.3f;
+            manganese=1.7;
+            name = "Unicron";
+            alloy = true;
+
+        }
+    }
+}
